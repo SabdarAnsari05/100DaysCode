af6d08ba5bb174470b0eed56169e111d5a089775
SabdarAnsari05 <sabdaransari0783@gmail.com>
2026-01-15 16:21:50 +0530

    code

---
+++ Array/Day4/RightRotateArrayBy1.java
@@ -0,0 +1,19 @@
+package Array.Day4;
+
+public class RightRotateArrayBy1 { // TC = O(n) & SC = O(1)
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 30, 40, 50 };
+
+        int temp = arr[arr.length - 1];
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[0] = temp;
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
